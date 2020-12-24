44219fbdb4d548c7e2a121f9a11feb56c5dbc28d
dewangga-pk <pacetcommunity570@gmail.com>
2020-12-24 22:31:27 +0700

    line formatting app & bottom bar layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="author" content="mironcoder">
-    <meta name="email" content="mironcoder@gmail.com">
-    <meta name="profile" content="https://themeforest.net/user/mironcoder">
-    <meta name="template" content="vegana">
-    <meta name="created" content="09-November-2020">
-    <meta name="title" content="vegana - vegan food ecommerce html template">
-    <meta name="keywords"
-          content="vegana, vegan, food, ecommerce, store, html, template, bootstrap, vegetarian, organic, vegetables, greengrocery, fruit, healthy, shop">
     <title>@yield('title')</title>
     <link rel="icon" href="{{asset('img/logo/favicon.png')}}">
     <link rel="stylesheet" href="{{asset('css/vendor/flaticon.css')}}">
